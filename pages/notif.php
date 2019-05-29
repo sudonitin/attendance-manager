6f06f311382b3f3bef7d6810dbb1dbaea9ffd8dd
globefire <sahun5774@gmail.com>
2019-05-29 21:45:40 +0530

    notification trial

--- pages/notif.php
+++ pages/notif.php
@@ -1,37 +1,52 @@
-<?php
-if (isset($_POST['as'])) {
-    # code...
-    $i = 1;
-    echo "<table class='table table-bordered' id='tab_logic' >
-    <thead class='thead-light' >
-      <tr>
-        <th scope='col'>#</th>
-        <th scope='col'>Time</th>
-        <th scope='col'>Mon</th>
-        <th scope='col'>Time</th>
-        <th scope='col'>Tue</th>
-        <th scope='col'>Time</th>
-        <th scope='col'>Wed</th>
-        <th scope='col'>Time</th>
-        <th scope='col'>Thu</th>
-        <th scope='col'>Time</th>
-        <th scope='col'>Fri</th>
-      </tr>
-    </thead>
-    <tbody>
-        <tr id='addr0'></tr>
-    </tbody>	
-    </table>" . $i ;
-    $i += 1;
-}   
-?>
-
 <html>
-<body>
-<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    <p></p>
-    <p id = ''>kjkjf</p>
-    <input type='submit' value='submit' name='as'/>
-</form>
-</body>
+    <body>
+        <a href="#" id='perm'>req perm</a>
+        <a href='#' id='trig'>trigg</a>
+
+        <script>
+            var perm = document.getElementById('perm');
+            var trig = document.getElementById('trig');
+
+            perm.addEventListener('click', function(e){
+                e.preventDefault();
+
+                if(!window.Notification){
+                    alert('not supported');
+                } else {
+                    Notification.requestPermission(function(p){
+                        if (p === 'denied') {
+                            alert('u hav denied notifications');
+                        } else if (p === 'granted') {
+                            alert('granted');
+                        }
+                    });
+                }
+            });
+
+            <?php $i = 'hello :) world'?>
+
+            trig.addEventListener('click', function(e){
+                var notify;
+                e.preventDefault();
+
+                if (Notification.permission === 'default') {
+                    alert('allow me')
+                } else {
+                    notify = new Notification("<?php echo $i; ?>");
+
+                }
+            });
+            
+            
+            setInterval(() => {
+                var d = new Date();
+                if (d.getHours() == 20 && d.getMinutes() == 47) {
+                    alert("mf");
+                }
+                    console.log('<?php echo $i?>');
+                }, 3000);
+
+        </script>
+
+    </body>
 </html>
