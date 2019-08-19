d2629750d3d5537129034c2d90d90eceb33326d6
globefire <sahun5774@gmail.com>
2019-08-19 11:53:42 +0530

    on signup notif working
    window.location.reload added in fetchdata of addrow.js

--- js/addrow.js
+++ js/addrow.js
@@ -75,6 +75,7 @@ $(document).ready(function(){
          {
            $('#container').html(data); 
            console.log('hello');
+           window.location.reload(); // reloaded the page
          }
        });     
       }
--- pages/fetch.php
+++ pages/fetch.php
@@ -66,7 +66,7 @@ else{
 		</table>
 
 <?php     
-
+  
     }
     
 
